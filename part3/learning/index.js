d366fbaa23d26af3bda1555c2233aec5121b2861
Qihua Han <qihua.han@icloud.com>
2023-02-27 10:47:19 +0800

    Modify part3\learning

--- part3/learning/index.js
+++ part3/learning/index.js
@@ -1,6 +1,7 @@
-const { response } = require('express')
 const express = require('express')
 const application = express()
+
+application.use(express.json())
 
 let notes = [
     {
@@ -24,7 +25,7 @@ let notes = [
 ]
 
 application.get('/', (request, response) => {
-    response.send('<h1>Hello World</n1>')
+    response.send('<h1>Hello</n1>')
 })
 
 application.get('/api/notes', (request, response) => {
@@ -41,13 +42,41 @@ application.get('/api/notes/:id', (request, response) => {
     }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+application.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
 })
 
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+
+application.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
 const PORT = 3001
 application.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
