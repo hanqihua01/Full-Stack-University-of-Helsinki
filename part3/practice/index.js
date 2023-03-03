bfa4bc2d8338e62daedaf4b418064a7c84f67e83
Qihua Han <qihua.han@icloud.com>
2023-03-03 09:11:49 +0800

    Delete practice3.12.

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.json())
